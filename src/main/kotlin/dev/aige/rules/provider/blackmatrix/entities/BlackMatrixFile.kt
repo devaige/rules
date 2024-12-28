a8a86009ad0302e60c524d49abd357df764350ea
Aige <aigestudio@hotmail.com>
2024-12-28 21:59:18 +0800

    修改规则

--- src/main/kotlin/dev/aige/rules/provider/blackmatrix/entities/BlackMatrixFile.kt
+++ src/main/kotlin/dev/aige/rules/provider/blackmatrix/entities/BlackMatrixFile.kt
@@ -17,10 +17,15 @@ data class BlackMatrixFile(private val filepath: String) {
                 val line: String = str.trim()
                 if (line.startsWith("#")) return@forEach
                 val parts: List<String> = line.split(",")
+
+                val type: ClashRule.Type = ClashRule.Type.from(parts[0])
+                val condition: String = parts[1]
+                // BlackMatrix 的这个 IPv6 规则好像有问题：
+                // IP-CIDR6,2001:4060:1:1005::10:32,no-resolve
+                if (type == ClashRule.Type.IP_CIDR6 && condition == "2001:4060:1:1005::10:32") return@forEach
                 rules.add(
                     ClashRule(
-                        ClashRule.Type.from(parts[0]),
-                        parts[1],
+                        type, condition,
                         if (parts.size > 2) parts[2].equals(ClashRule.DNS_RESOLVE, true) else false,
                     )
                 )
